827b9735d37c4713002a8621e578d79dc0afa961
kucbkto <95414625+kucbkto@users.noreply.github.com>
2022-04-28 15:08:00 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -7,7 +7,7 @@ from telethon import events, Button, TelegramClient
 from pyrogram import idle
 from main.plugins.main import Bot, userbot
 
-st = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @dsrs_group\n**DEV:** @kashir_bots"
+st = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @dsrs_group\n**DEV:** @@The_dsr"
 
 @bot.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
